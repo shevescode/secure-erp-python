ad645e16e4042092917da8b6a13e66df8dfdedef
wikuuu1 <wiktoria.rajba391@gmail.com>
2021-11-11 18:36:35 +0100

    Added get_biggest_revenue_product(),update_transaction(), delete_transaction(), change_transaction_data(data, label), change_ids(id, label) and comments

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -7,13 +7,14 @@ from controller import crm_controller
 import os
 
 
-def list_transactions():
-    """Reads list of sales from a file and prints it as a table"""
+def list_of_transactions():
+    """Reads list of sales transactions from a file and prints it as a table"""
     table = data.read_table_from_file("model/sales/sales.csv", separator=';')
     print(tabulate(table, headers=sales.HEADERS, tablefmt='fancy_grid', showindex=True))
 
 
 def add_transaction():
+    """Allows for adding new transaction, overwrites a file and prints new table with complete list of transactions"""
     table = data.read_table_from_file("model/sales/sales.csv", separator=';')
     row = view.get_inputs("sales")
     table.append(row)
@@ -29,6 +30,7 @@ def add_transaction():
     """)
     os.system('pause')
     view.console_clear()
+
 
 def get_customer_id_for_transaction():
     """Reads file and returns the ID and Name of customers"""
@@ -58,15 +60,47 @@ def get_customer_id_for_transaction():
 
 
 def update_transaction():
-    view.print_error_message("Not implemented yet.")
+    """Allows for updating any transaction from list and overwrites it in file"""
+    view.console_clear()
+    list_of_transactions()
+    table = data.read_table_from_file("model/sales/sales.csv", separator=';')
+    updating_transaction = int(view.get_input(
+        "Which transaction datas would you like to update?"))
+
+    for index, value in enumerate(table):
+        if updating_transaction == index:
+            value[0] = sales.change_ids(value[0], "transaction_id")
+            value[1] = sales.change_ids(value[1], "customer_id")
+            data.write_table_to_file("model/crm/crm.csv", table, separator=';')
+            value[2] = sales.change_transaction_data(value[2], "product")
+            value[3] = sales.change_transaction_data(value[3], "price")
+            value[4] = sales.change_transaction_data(value[4], "transaction_date")
+    data.write_table_to_file("model/sales/sales.csv", table, separator=';')
 
 
 def delete_transaction():
-    view.print_error_message("Not implemented yet.")
+    """Allows to choose transaction to delete and removes it from file"""
+    view.console_clear()
+    list_of_transactions()
+    table = data.read_table_from_file("model/sales/sales.csv", separator=';')
+    operation = int(view.get_input("Which transaction would you like to delete?"))
+    for index, transaction  in enumerate(table):
+        if operation == index:
+            option = input("Are you sure? Enter 'yes' or 'no'\n")
+            if option == "yes":
+                table.remove(transaction)
+                data.write_table_to_file(
+                    "model/sales/sales.csv", table, separator=';')
+            elif option == "no":
+                break
+    view.console_clear()
+    list_of_transactions()
+
+
 
 
 def get_biggest_revenue_transaction():
-
+    """Reads file and returns the transaction with the biggest revenue"""
     table = data.read_table_from_file("model/sales/sales.csv", separator=';')
     x = 0
     y = 0
@@ -85,9 +119,25 @@ def get_biggest_revenue_transaction():
     """)
     os.system("pause")
 
-def get_biggest_revenue_product():
-    view.print_error_message("Not implemented yet.")
 
+def get_biggest_revenue_product():
+    """Reads file and returns product name and price with the biggest achieved revenue """
+    table = data.read_table_from_file("model/sales/sales.csv", separator=';')
+    product = 0
+    cost = 0
+
+    for index, value in enumerate(table):
+        if float(value[3]) > cost:
+            product = index
+            cost = float(value[3])
+
+    view.console_clear()
+    print(f"""The biggest revenue has been achived thanks to product:
+
+    product: {table[product][2]}
+    price: {table[product][3]}
+    """)
+    os.system("pause")
 
 def count_transactions_between():
     view.print_error_message("Not implemented yet.")
@@ -98,8 +148,9 @@ def sum_transactions_between():
 
 
 def run_operation(option):
+    """Invokes (calls for) functions depending on user's input"""
     if option == 1:
-        list_transactions()
+        list_of_transactions()
     elif option == 2:
         add_transaction()
     elif option == 3:
@@ -121,6 +172,7 @@ def run_operation(option):
 
 
 def display_menu():
+    """Displays menu options"""
     options = ["Back to main menu",
                "List transactions",
                "Add new transaction",
@@ -134,11 +186,13 @@ def display_menu():
 
 
 def menu():
+    """ʕ•ᴥ•ʔ ʕ•ᴥ•ʔExecutes menu...... ᶘᵒᴥᵒᶅ ........... ( ͡° ͜ʖ ͡°)
+    [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] ◕‿◕ [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] ◕‿◕ [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]"""
     operation = None
     while operation != '0':
         display_menu()
         try:
-            operation = view.get_input("Select an operation")
+            operation = view.get_input("Select an operation [̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]")
             view.console_clear()
             run_operation(int(operation))
         except KeyError as err:
--- model/sales/sales.py
+++ model/sales/sales.py
@@ -11,6 +11,77 @@ Data table structure:
 from typing import MutableMapping
 from model import data_manager, util
 from view import terminal as view
+import os
+
+def change_ids(id, label):
+    """Prints actual transactio id and customer id, asks for new id's and returns new id's to be changed in file"""
+    while True:
+        view.console_clear()
+        print(f"Actual {label} is {id}")
+        user_input = input(
+            f"Would you like to set a new {label}? Enter 'yes' or 'no'\n").lower()
+        if user_input == "yes":
+            view.console_clear()
+            print(f"Actual {label} is {id}")
+            if label == "transaction_id":
+                new_id = util.generate_id()
+            elif label == "customer_id":
+                new_id = util.generate_id()
+
+            while True:
+                view.console_clear()
+                print(f"Actual {label} is {id}")
+                print(f"New {label} is {new_id}")
+                option = input(
+                    f"Would you like to overwrite the old {label}? Enter 'yes' or 'no'\n")
+                if option == "yes":
+                    return new_id
+                elif option == "no":
+                    return id
+                else:
+                    continue
+
+        elif user_input == "no":
+            return id
+        else:
+            continue
+
+def change_transaction_data(data, label):
+    """Prints actual data, asks for new transaction data and returns new transaction data to be changed in file"""
+    while True:
+        view.console_clear()
+        print(f"Actual {label} is {data}")
+        user_input = input(
+            f"Would you like to set a new {label}? Enter 'yes' or 'no'\n").lower()
+        if user_input == "yes":
+            view.console_clear()
+            print(f"Actual {label} is {data}")
+            if label == "product":
+                new_data = view.set_data("Write product name: ")
+            elif label == "price":
+                new_data = view.set_data("Write new price of the product: ")
+            elif label == "transaction_date":
+                new_data = view.set_data(
+                    "Write transaction date: ")
+
+            while True:
+                view.console_clear()
+                print(f"Actual {label} is {data}")
+                print(f"New {label} is {new_data}")
+                option = input(
+                    f"Would you like to overwrite the old {label}? Enter 'yes' or 'no'\n")
+                if option == "yes":
+                    return new_data
+                elif option == "no":
+                    return data
+                else:
+                    continue
+
+        elif user_input == "no":
+            return data
+        else:
+            continue
+
 
 
 DATAFILE = "model/sales/sales.csv"
